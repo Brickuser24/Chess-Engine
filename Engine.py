98e531b1cd0f1c8149de9d8c10905672c00967d9
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2025-06-01 21:39:43 +0530

    Update Engine.py
    Add Negamax and opening book

--- Engine.py
+++ Engine.py
@@ -1,4 +1,10 @@
 import chess
+import chess.polyglot
+
+reader = chess.polyglot.open_reader("komodo.bin")
+book_moves = True
+
+Checkmate_Score = 10000
 
 def order_moves(board):
     promotions = []
@@ -18,78 +24,71 @@ def order_moves(board):
 
     return promotions + captures + checks + regular
 
-def evaluate(board,depth):
-
-    if board.turn == chess.BLACK and board.is_checkmate():
-        return 10000 + depth
-    if board.turn == chess.WHITE and board.is_checkmate():
-        return -10000 - depth
-    if board.is_game_over(claim_draw=True) and not board.is_checkmate():
-        return 0
-
+def evaluate(board):
+    
     eval = 0
     piece_values = {"P": 1, "N": 3, "B": 3, "R": 5, "Q": 9, "K": 0, "p": -1, "n": -3, "b": -3, "r": -5, "q": -9, "k": 0}
     for square in board.piece_map():
         piece = board.piece_at(square)
         if piece:
             eval += piece_values[piece.symbol()]
-    return eval
 
-def minimax(board,depth,alpha,beta):
-
-    if depth == 0 or board.is_game_over():
-        return evaluate(board, depth)
-    
     if board.turn:
-        best = float('-inf')
-        for move in order_moves(board):
-            board.push(move)
-            eval = minimax(board,depth-1,alpha,beta)
-            board.pop()
-            best = max(eval,best)
-            alpha = max(alpha,eval)
-            if beta <= alpha:
-                break
-    
+        return eval
     else:
-        best = float('inf')
-        for move in order_moves(board):
-            board.push(move)
-            eval = minimax(board,depth-1,alpha,beta)
-            board.pop()
-            best = min(eval,best)
-            beta = min(beta, eval)
-            if beta <= alpha:
-                break
-    
-    return best
+        return -eval
 
-def search(board,depth):
+def negamax(board, depth, alpha, beta):
+
+    if board.is_game_over(claim_draw = True):
+        if board.is_checkmate():
+            return -Checkmate_Score - depth
+        else:
+            return 0
+
+    if depth == 0:
+        return evaluate(board)
+    
+    max_eval = float('-inf')
+    
+    for move in order_moves(board):
+        board.push(move)
+        eval = -negamax(board, depth - 1, -beta, -alpha)
+        board.pop()
+        
+        max_eval = max(max_eval, eval)
+        alpha = max(alpha, eval)
+        
+        if beta <= alpha:
+            break
+            
+    return max_eval
+
+def search(board, depth):
+
+    global book_moves
+    if book_moves:
+        try:
+            book_move = reader.weighted_choice(board)
+            return book_move.move
+        except:
+            book_moves = False
+            reader.close()
 
     best_move = None
+    max_eval = float('-inf')
     alpha = float('-inf')
     beta = float('inf')
 
-    if board.turn:
-        best = float('-inf')
-        for move in order_moves(board):
-            board.push(move)
-            eval = minimax(board,depth-1,alpha,beta)
-            board.pop()
-            if eval > best:
-                best = eval
-                best_move = move
-            alpha = max(alpha, eval)
+    for move in order_moves(board):
+        board.push(move)
+        eval = -negamax(board, depth - 1, -beta, -alpha)
+        board.pop()
 
-    else:
-        best = float('inf')
-        for move in order_moves(board):
-            board.push(move)
-            eval = minimax(board,depth-1,alpha,beta)
-            board.pop()
-            if eval < best:
-                best = eval   
-                best_move = move
-            beta = min(beta, eval)
-    
+        if eval > max_eval:
+            max_eval = eval
+            best_move = move
+        
+        alpha = max(alpha, eval)
+
     return best_move
